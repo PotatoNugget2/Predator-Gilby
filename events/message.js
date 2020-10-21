de26a3338474ced6bebf1fd01886ea906351f689
DrPepper <josephcrain14@gmail.com>
2020-10-21 13:47:33 -0400

    react to message

--- events/message.js
+++ events/message.js
@@ -3,6 +3,7 @@ triggerWords['hey'] = {text: `Hello! :slight_smile: `};
 triggerWords['sorry'] = {text: `It's okay, I forgive you! :smiling_face_with_3_hearts: `};
 triggerWords['gilby'] = {words: 1, text: `Whats up! How are you doing? :sunglasses: `};
 triggerWords['^'] = {words: 1, text: `^`};
+triggerWords['goodboy'] = { words: 1, text: `*pants*` };
 
 module.exports = async (client, msg) => {
     // Check for bot and disallow
@@ -18,7 +19,9 @@ module.exports = async (client, msg) => {
         let trigger = triggerWords[triggers[0]];
         // If the length of the word count is above our set trigger then return
         if (words.length > trigger.words) { return; }
-        // Assuming we didn't get returned above we send the message
-        msg.channel.send(trigger.text)
+        // Assuming we didn't get returned above we will send our trigger text
+        // with a reaction
+        msg.react(config.response_reaction);
+        msg.channel.send(trigger.text);
     }
 }
